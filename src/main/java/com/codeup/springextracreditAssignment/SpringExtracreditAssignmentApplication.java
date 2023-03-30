89d3ff1903c44d1cf16fba0fe2753dcd92b75695
saidshah <saidmshah93@gmail.com>
2023-03-30 14:01:27 -0500

    first commit

---
+++ src/main/java/com/codeup/springextracreditAssignment/SpringExtracreditAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.springextracreditAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExtracreditAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExtracreditAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/springextracreditAssignment/SpringExtracreditAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.springextracreditAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExtracreditAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
